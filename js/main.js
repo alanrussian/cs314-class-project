391582717e44d443dd6727796153b5aa0be68b53
Alan Russian <arussian@iwu.edu>
2014-04-14 08:20:28 -0500

    Added empty table message.

--- js/main.js
+++ js/main.js
@@ -1,93 +1,126 @@
-$(function() {
-    $(".results .delete").click(function() {
-        if (! confirm("Are you sure that you want to delete this?")) {
-            return;
-        }
+(function() {
+    var emptyMessage = "There are no records.";
 
-        // Gather elements
-        var row = $(this).closest("tr");
+    var addEmptyMessage = function(table) {
+        var $table = $(table);
 
-        // Gather information
-        var sqlTable = $(this).data("table");
-        if (sqlTable == undefined) {
-            console.error("Need to specify table.");
-            return;
-        }
+        var columns = $table.find("thead tr th").length;
 
-        // Get arguments
-        var args = {};
-        row.find("td").each(function(i, element) {
-            $element = $(element);
+        $table.find("tbody").append(
+            $("<tr>").append(
+                $("<td>")
+                    .text(emptyMessage)
+                    .attr("colSpan", columns)
+            )
+        );
+    };
 
-            // If it has the pk data element, add it as an argument
-            var pk = $element.data("pk");
-            if (pk != undefined) {
-                args[pk] = $element.text();
+    $(function() {
+        // Add empty message if applicable
+        $(".results").each(function(i, table) {
+            if ($(table).find("tbody tr").length === 0) {
+                addEmptyMessage(table);
             }
         });
 
-        var allData = $(this).data();
-        for (var key in allData) {
-            if (key.substr(0, 2) === "pk" && key.length > 2) {
-                args[key.substr(2).toLowerCase()] = allData[key];
+        $(".results .delete").click(function() {
+            if (! confirm("Are you sure that you want to delete this?")) {
+                return;
             }
-        }
 
-        var data = {
-            table: sqlTable,
-            args: args
-        };
+            // Gather elements
+            var row = $(this).closest("tr");
 
-        $.post("delete.php", data)
-            .done(function(response) {
-                if (response == "1") {
-                    row.remove();
-                } else {
-                    alert("Could not delete.\n\n"+ response);
+            // Gather information
+            var sqlTable = $(this).data("table");
+            if (sqlTable == undefined) {
+                console.error("Need to specify table.");
+                return;
+            }
+
+            // Get arguments
+            var args = {};
+            row.find("td").each(function(i, element) {
+                $element = $(element);
+
+                // If it has the pk data element, add it as an argument
+                var pk = $element.data("pk");
+                if (pk != undefined) {
+                    args[pk] = $element.text();
                 }
-            })
-            .fail(function(event, status, response) {
-                alert("Could not delete.\n\n"+ response);
             });
-    });
 
-    $("#addArtistMusician .save").click(function() {
-        var dialog = $("#addArtistMusician");
-
-        var form = dialog.find("form");
-
-        $.post("add_artist_musician.php", form.serialize(), 'json')
-            .done(function(response) {
-                if (typeof response === 'string') {
-                    alert("Could not add.\n\n"+ response);
-                    return;
+            var allData = $(this).data();
+            for (var key in allData) {
+                if (key.substr(0, 2) === "pk" && key.length > 2) {
+                    args[key.substr(2).toLowerCase()] = allData[key];
                 }
+            }
 
-                // If I had more time, I would do add the row to the table here
-                location.reload();
-            })
-            .fail(function(event, status, response) {
-                alert("Could not add.\n\n"+ response);
-            })
-    });
+            var data = {
+                table: sqlTable,
+                args: args
+            };
 
-    $("#addAlbumSong .save").click(function() {
-        var dialog = $("#addAlbumSong");
+            $.post("delete.php", data)
+                .done(function(response) {
+                    if (response == "1") {
+                        var table = row.closest("table");
 
-        var form = dialog.find("form");
+                        // Remove row
+                        row.remove();
 
-        $.post("add_album_song.php", form.serialize(), 'json')
-            .done(function(response) {
-                if (typeof response === 'string') {
+                        // If table is now empty, add the empty message
+                        if (table.find("tbody tr").length === 0) {
+                            addEmptyMessage(table);
+                        }
+                    } else {
+                        alert("Could not delete.\n\n"+ response);
+                    }
+                })
+                .fail(function(event, status, response) {
+                    alert("Could not delete.\n\n"+ response);
+                });
+        });
+
+        $("#addArtistMusician .save").click(function() {
+            var dialog = $("#addArtistMusician");
+
+            var form = dialog.find("form");
+
+            $.post("add_artist_musician.php", form.serialize(), 'json')
+                .done(function(response) {
+                    if (typeof response === 'string') {
+                        alert("Could not add.\n\n"+ response);
+                        return;
+                    }
+
+                    // If I had more time, I would do add the row to the table here
+                    location.reload();
+                })
+                .fail(function(event, status, response) {
                     alert("Could not add.\n\n"+ response);
-                    return;
-                }
+                })
+        });
 
-                // If I had more time, I would do add the row to the table here
-                location.reload();
-            })
-            .fail(function(event, status, response) {
-                alert("Could not add.\n\n"+ response);
-            })
+        $("#addAlbumSong .save").click(function() {
+            var dialog = $("#addAlbumSong");
+
+            var form = dialog.find("form");
+
+            $.post("add_album_song.php", form.serialize(), 'json')
+                .done(function(response) {
+                    if (typeof response === 'string') {
+                        alert("Could not add.\n\n"+ response);
+                        return;
+                    }
+
+                    // If I had more time, I would do add the row to the table here
+                    location.reload();
+                })
+                .fail(function(event, status, response) {
+                    alert("Could not add.\n\n"+ response);
+                })
+        });
     });
-});
+})();
